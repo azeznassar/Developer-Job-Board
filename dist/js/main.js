af9b17b687eacf1f969ff86f936e321e48a71fb3
Azez Nassar <azez.nassar@yahoo.com>
2018-08-07 21:21:24 +1200

    generated cards based on api call

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,69 @@
 const script = document.querySelector('.api');
-const list = document.querySelector('.list');
+const container = document.querySelector('.container');
 
 function jsonp(jobs) {
 	jobs.forEach(job => {
-		//console.log(data);
-		card = document.createElement("div");
+		//create card and append to container
+		card = document.createElement('div');
 		card.classList.add('card');
-		list.appendChild(card);
+		container.appendChild(card);
 
-		jobListing = document.createElement("h2");
-        jobListing.textContent = `${job.title} in ${job.location}`
-		card.appendChild(jobListing);
+		//create a wrapper and append to card
+		cardWrapper = document.createElement('a')
+		cardWrapper.href = '#';
+		cardWrapper.classList.add('cardWrapper');
+		card.appendChild(cardWrapper);
+
+		//create inner card and append to wrapper
+		innerCard = document.createElement('div');
+		innerCard.classList.add('innerCard');
+		cardWrapper.appendChild(innerCard);
+
+		//create img and append to innerCard
+		cardImg = document.createElement('img');
+		cardImg.alt = "Company Logo";
+
+		job.company_logo !== null ? cardImg.src = job.company_logo : cardImg.src = 'img/logoPlaceholder.jpg';
+		innerCard.appendChild(cardImg);
+
+		//create card header and append to innerCard
+		jobTitle = document.createElement('h3');
+
+		if (job.title.length > 36) {
+
+			let title = job.title.slice(0,34);
+			title += '...'
+			jobTitle.textContent = title;
+		} else {
+			jobTitle.textContent = job.title;
+		}
+		innerCard.appendChild(jobTitle);
+
+
+		//create info div and append to innerCard
+		cardInfo = document.createElement('div');
+		cardInfo.classList.add('info');
+		innerCard.appendChild(cardInfo);
+
+		//create h4s and append to info div
+		jobCompany = document.createElement('h4');
+		jobLocation = document.createElement('h4');
+		jobCompany.textContent = job.company;
+		jobLocation.textContent = job.location;
+		cardInfo.appendChild(jobCompany);
+		cardInfo.appendChild(jobLocation);
+
+		//create bottomWrapper div
+		bottomWrapper = document.createElement('div');
+		bottomWrapper.classList.add('bottomWrapper');
+		innerCard.appendChild(bottomWrapper);
+
+		//create button and i and append into bottomWrapper
+		cardBtn = document.createElement('button');
+		bottomWrapper.appendChild(cardBtn);
+		cardArrow = document.createElement('i');
+		cardArrow.innerHTML = '<i class="fas fa-arrow-right"></i>';
+		cardBtn.appendChild(cardArrow);
 	});
 }
 
