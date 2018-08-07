dbb86b14919eb448e0f6f4e4cbcaed076a04c78d
Azez Nassar <azez.nassar@yahoo.com>
2018-08-07 18:10:41 +1200

    setup node-sass

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,18 @@
-
 const script = document.querySelector('.api');
 const list = document.querySelector('.list');
 
 function jsonp(jobs) {
 	jobs.forEach(job => {
 		//console.log(data);
-		count = 0;
 		card = document.createElement("div");
 		card.classList.add('card');
 		list.appendChild(card);
 
-		count += 1;
-		jobListing = document.createElement("p");
-        jobListing.textContent = `Job ${count}: ${job.title} in ${job.location}`
+		jobListing = document.createElement("h2");
+        jobListing.textContent = `${job.title} in ${job.location}`
 		card.appendChild(jobListing);
 	});
 }
-
 
 
 
